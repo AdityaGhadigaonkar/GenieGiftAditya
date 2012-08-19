25463c7336f5f2293d762075a9d636d9777b5038
Aditya Ghadigaonkar <aditya.ghadigaonkar70@gmail.com>
2012-08-19 16:35:56 +0530

    Commit .

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="jquery-1.3.2.min.js"></script>
+
+<title>Welcome to Genie Gift Me!</title>
+
+</head>
+
+<body>
+<a href="edit_wishes.php" >Manage your wishlist &rarr;</a>
+<?php
+include 'create_wish.php'; 
+?>
+</body>
+</html>
